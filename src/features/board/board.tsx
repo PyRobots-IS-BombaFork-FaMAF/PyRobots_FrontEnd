3d3d52d4044ae661554a4fdf8f8c0bfe898d20ae
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-09 16:43:12 -0300

    PYR-53 Margin size modulariced

--- src/features/board/board.tsx
+++ src/features/board/board.tsx
@@ -2,16 +2,17 @@ import { Stage, Layer, Rect } from 'react-konva';
 
 function Board() {
 
-  const window_min_size = Math.min(window.innerWidth, window.innerHeight)
+  const window_min_size: number = Math.min(window.innerWidth, window.innerHeight)
+  const margin_percentage: number = 0.025
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
         <Rect
-          x={window_min_size * 0.025}
-          y={window_min_size * 0.025}
-          width={window_min_size * 0.95}
-          height={window_min_size * 0.95}
+          x={window_min_size * margin_percentage}
+          y={window_min_size * margin_percentage}
+          width={window_min_size * (1 - 2 * margin_percentage)}
+          height={window_min_size * (1 - 2 * margin_percentage)}
           fill="white"
           shadowBlur={10}
         />
