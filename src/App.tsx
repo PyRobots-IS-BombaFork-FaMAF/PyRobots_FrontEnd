f31db082b7ce48039989c7a06d42389b2dafa5e1
Santiagocoggiola <santiago.coggiola@mi.unc.edu.ar>
2022-10-09 15:20:59 -0300

    Added missing imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<SignUp/>}/>
-                    <Route path="/Login"/>
-                </Routes>
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/Login" />
+        </Routes>
       </BrowserRouter>
     </div>
   );
