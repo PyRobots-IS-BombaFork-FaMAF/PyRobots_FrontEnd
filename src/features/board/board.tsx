cc1d0d81f6d3e7fc0a4ef0bc02e6883d43268384
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-10 18:27:08 -0300

    Add title to board

--- src/features/board/board.tsx
+++ src/features/board/board.tsx
@@ -1,5 +1,5 @@
-import { Stage, Layer, Rect } from 'react-konva';
-import { boardConfig } from './boardHelper';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import { boardConfig, sideTextConfig } from './boardHelper';
 
 function BackGround(board: boardConfig) {
   return (
@@ -15,18 +15,41 @@ function BackGround(board: boardConfig) {
 }
 
 
+function SideText(sideText: sideTextConfig) {
+  const { x0, y0, x1, y1} = sideText;
+  return (
+    <Text
+      x={x0}
+      y={y0}
+      width={x1}
+      height={y1}
+      fill="black"
+      text="Simulación"
+      fontSize={50}
+    />
+  );
+}
+
+
 function Board() {
 
   const window_min_size: number = Math.min(window.innerWidth, window.innerHeight)
   const margin_percentage: number = 0.025
+  const margin: number = window_min_size * margin_percentage
 
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
         <BackGround
-          x0={window_min_size * margin_percentage}
-          y0={window_min_size * margin_percentage}
+          x0={margin}
+          y0={margin}
           size={window_min_size * (1 - 2 * margin_percentage)}
+        />
+        <SideText
+          x0={window_min_size}
+          y0={margin}
+          x1={window.innerWidth - margin}
+          y1={window.innerHeight - margin}
         />
       </Layer>
     </Stage>
--- src/features/board/boardHelper.ts
+++ src/features/board/boardHelper.ts
@@ -1,3 +1,3 @@
 
 export type boardConfig = { x0: number, y0: number, size: number }
-
+export type sideTextConfig = { x0: number, y0: number, x1: number, y1: number }
