ea2f5f746b167af54a19a74de8202e3c6e4dc8dd
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-15 20:31:37 -0300

    PYR-43 Use `Container` in `NewGame`

--- src/features/newGame/NewGame.tsx
+++ src/features/newGame/NewGame.tsx
@@ -1,28 +1,27 @@
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import { createTheme, ThemeProvider, Theme } from "@mui/material/styles";
-
 
 function GameForm() {
   return (
     <form>
-      <Grid item xs={12} >
+      <Grid>
         <TextField required id="game-name" label="Nombre" variant="standard" helperText="Entre 3 y 12 caracteres" />
       </Grid>
-      <Grid item xs={12} >
+      <Grid>
         <TextField id="games-amount" label="Cantidad de simulaciones" variant="standard" helperText="Entre 1 y 200" />
       </Grid>
-      <Grid item xs={12} >
+      <Grid>
         <TextField id="rounds-amount" label="Cantidad de rondas" variant="standard" helperText="Entre 1 y 10000" />
       </Grid>
-      <Grid item xs={12} >
+      <Grid>
         <TextField id="max-players" label="Máximo de jugadores" variant="standard" helperText="Entre 2 y 4" />
       </Grid>
-      <Grid item xs={12} >
+      <Grid>
         <TextField id="min-players" label="Mínimo de jugadores" variant="standard" helperText="Entre 2 y 4" />
       </Grid>
-      <Grid item xs={12} >
+      <Grid>
         <TextField id="password" label="Contraseña" variant="standard" helperText="Entre 8 y 16 caracteres" />
       </Grid>
       <Button
@@ -39,22 +38,14 @@ function GameForm() {
 
 
 function NewGame() {
-  // blue theme
-  const theme: Theme = createTheme({
-    palette: {
-      primary: {
-        main: "#1976d2",
-      },
-    },
-  });
 
   return (
-    <ThemeProvider theme={createTheme(theme)}>
+    <Container>
       <div>
         <h1>Crear partida</h1>
         <GameForm />
       </div>
-    </ThemeProvider>
+    </Container>
   );
 };
 
