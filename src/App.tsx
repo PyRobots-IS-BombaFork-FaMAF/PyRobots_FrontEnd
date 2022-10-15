37516f340051afebb7d8b9de2b4bce698054258c
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-15 19:25:54 -0300

    PYR-43 Form for creating a game

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Board from "./features/board/board";
 import SignUp from "./features/register/SignUp";
+import NewGame from "./features/newGame/NewGame";
 
 export const API = "http://127.0.0.1:8000/";
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/tableroDePrueba/" element={<Board />} />
           <Route path="/" element={<SignUp />} />
           <Route path="/Login" />
+          <Route path="/newGame" element={<NewGame />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/features/newGame/NewGame.tsx
@@ -0,0 +1,42 @@
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+
+
+function GameForm() {
+  return (
+    <form>
+      <Grid item xs={12} >
+        <TextField required id="game-name" label="Nombre" variant="standard" />
+      </Grid>
+      <Grid item xs={12} >
+        <TextField id="games-amount" label="Cantidad de simulaciones" variant="standard" />
+      </Grid>
+      <Grid item xs={12} >
+        <TextField id="rounds-amount" label="Cantidad de rondas" variant="standard" />
+      </Grid>
+      <Grid item xs={12} >
+        <TextField id="max-players" label="Máximo de jugadores" variant="standard" />
+      </Grid>
+      <Grid item xs={12} >
+        <TextField id="min-players" label="Mínimo de jugadores" variant="standard" />
+      </Grid>
+      <Grid item xs={12} >
+        <TextField id="password" label="Contraseña" variant="standard" />
+      </Grid>
+      <input type="submit" value="Crear robot"></input>
+    </form>
+  );
+};
+
+
+function NewGame() {
+  return (
+    <div>
+      <div id="bg-image"> </div>
+      <GameForm />
+    </div>
+  );
+};
+
+
+export default NewGame;
