32a9f8169d5997d19e3872facff20a232f9d3c70
Santiagocoggiola <santiago.coggiola@mi.unc.edu.ar>
2022-10-16 01:48:58 -0300

    PYR-45 listMatches template

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import Board from "./features/board/board";
 import SignUp from "./features/register/SignUp";
+import ListMatches from "./features/listMatches/ListMatches";
 
 export const API = "http://127.0.0.1:8000/";
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/tableroDePrueba/" element={<Board />} />
           <Route path="/" element={<SignUp />} />
           <Route path="/Login" />
+          <Route path="/listMatches" element={<ListMatches/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/features/listMatches/ListMatches.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import PersonIcon from '@mui/icons-material/Person';
+import { Container } from '@mui/system';
+
+export default function ListMatches() {
+
+
+
+
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper', marginTop: 30,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"}}>
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <PersonIcon/>
+                </ListItemIcon>
+                <ListItemText primary="Drafts" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+        <Divider />
+        <nav aria-label="secondary mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Trash" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton component="a" href="#simple-list">
+                <ListItemText primary="Spam" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
+    </Container>
+  );
+}
