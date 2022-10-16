b56e0c748ee190136e51b0a664f5996d91dfc2c6
Franco Artico <francoartico0@gmail.com>
2022-10-16 01:21:56 -0300

    PYR-40 Submit form data

--- src/features/newrobot/CreateRobot.tsx
+++ src/features/newrobot/CreateRobot.tsx
@@ -1,49 +1,57 @@
 import TextField from "@mui/material/TextField";
+import postRobot from "./CreateRobotApi";
 import "./CreateRobot.css";
 
-  const handleChange = (e: any) => {
-    console.log(e);
-  }
-
-function InputFile({ idName, label }: any) {
+function InputFile({ label }: any) {
   return (
     <div>
-      <label className="label-file" htmlFor={idName}>
+      <label className="label-file" htmlFor="robot-code">
         {" "}
         {label}{" "}
       </label>
-      <input id={idName} type="file" required />
+      <input id="robot-code" name="robotCode" type="file" required />
     </div>
   );
 }
 
 const AvatarRobot = (): any => (
-  <img className="avatar" src="https://robohash.org/user1" alt="robotAvatar" />
+  <img id="avatar" src="https://robohash.org/user1" alt="robotAvatar" />
 );
 
 const ButtonChangeAvatar = (): any => (
   <div className="div-image">
     <p className="botton-text">Subir una foto </p>
-    <input className="input-avatar" type="file" onChange={handleChange} />
+    <input name="avatarRobot" className="input-avatar" type="file" />
   </div>
-);
-
-const RobotForm = (): any => (
-  <form className="robot-form">
-    <AvatarRobot />
-    <ButtonChangeAvatar />
-    <TextField required id="robot-name" label="Nombre del Robot" variant="standard" />
-    <InputFile idName="robot-code" label="Archivo .py para el robot" />
-    <input type="submit" value="Crear robot"></input>
-  </form>
 );
 
 const CreateRobot = () => {
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    postRobot(data);
+  };
+
   return (
     <div>
       <div id="bg-image"> </div>
-      <RobotForm />
+      <form name="robotForm" className="robot-form" onSubmit={handleSubmit}>
+        <AvatarRobot />
+        <ButtonChangeAvatar />
+        <TextField
+          required
+          name="robotName"
+          label="Nombre del Robot"
+          variant="standard"
+          inputProps={{ maxLength: 12, minLength: 3 }}
+        />
+        <InputFile label="Archivo .py para el robot" />
+        <input
+          type="submit"
+          value="Crear robot"
+        ></input>
+      </form>
     </div>
   );
 };
