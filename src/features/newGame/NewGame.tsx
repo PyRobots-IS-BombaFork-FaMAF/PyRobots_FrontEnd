987cb07dff7a94045e830ea079610f8d8ff75360
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-17 09:48:14 -0300

    PYR-43 Move validation regex outside functions and export them (por testing)

--- src/features/newGame/NewGame.tsx
+++ src/features/newGame/NewGame.tsx
@@ -17,22 +17,22 @@ function onSubmit_newGame(event: React.FormEvent<HTMLFormElement>) {
   }
 
   const rounds = data.get('rounds-amount')
-  if (typeof(rounds) === 'string') {
+  if (typeof (rounds) === 'string') {
     newGameInfo.rounds = parseInt(rounds)
   }
 
   const games = data.get('games-amount')
-  if (typeof(games) === 'string') {
+  if (typeof (games) === 'string') {
     newGameInfo.games = parseInt(games)
   }
 
   const max_players = data.get('max-players')
-  if (typeof(max_players) === 'string') {
+  if (typeof (max_players) === 'string') {
     newGameInfo.max_players = parseInt(max_players)
   }
 
   const min_players = data.get('min-players')
-  if (typeof(min_players) === 'string') {
+  if (typeof (min_players) === 'string') {
     newGameInfo.min_players = parseInt(min_players)
   }
 
@@ -41,60 +41,65 @@ function onSubmit_newGame(event: React.FormEvent<HTMLFormElement>) {
 
 }
 
+// Regex of input validation (in string because `pattern` requires a string)
+export const game_name_regex: string = "^.{3,12}$"
+export const games_amount_regex: string = "^([1-9][0-9]?|1[0-9]{2}|200)$"
+export const rounds_amount_regex: string = "^([1-9][0-9]{0,3}|10000)$"
+export const max_players_regex: string = "^[2-4]$"
+export const min_players_regex: string = "^[2-4]$"
+export const password_regex: string = "^(.{8,16}|)$"
+
 
 function GameForm() {
-
-  
-  
   return (
-  <Container>
-    <Box component="form" onSubmit={onSubmit_newGame} >
-      <Grid>
-        <TextField required
-          name="game-name" label="Nombre" variant="standard" helperText="Entre 3 y 12 caracteres"
-          type="text" inputProps={{ maxLength:12, pattern: "^.{3,12}$" }}
-        />
-      </Grid>
-      <Grid>
-        <TextField required
-          name="games-amount" label="Cantidad de simulaciones" variant="standard" helperText="Entre 1 y 200"
-          defaultValue="200" type="text" inputProps={{ maxLength: 3, pattern: "^([1-9][0-9]?|1[0-9]{2}|200)$" }}
-        />
-      </Grid>
-      <Grid>
-        <TextField required
-          name="rounds-amount" label="Cantidad de rondas" variant="standard" helperText="Entre 1 y 10000"
-          defaultValue="10000" type="text" inputProps={{ maxLength: 5, pattern: "^([1-9][0-9]{0,3}|10000)$" }}
-        />
-      </Grid>
-      <Grid>
-        <TextField required
-          name="max-players" label="Máximo de jugadores" variant="standard" helperText="Entre 2 y 4"
-          defaultValue="4" type="text" inputProps={{ maxLength: 1, pattern: "^[2-4]$" }}
-        />
-      </Grid>
-      <Grid>
-        <TextField required
-          name="min-players" label="Mínimo de jugadores" variant="standard" helperText="Entre 2 y 4"
-          defaultValue="2" type="text" inputProps={{ maxLength: 1, pattern: "^[2-4]$" }}
-        />
-      </Grid>
-      <Grid>
-        <TextField
-          name="password" label="Contraseña" variant="standard" helperText="Entre 8 y 16 caracteres"
-          type="text" inputProps={{ maxLength: 16, pattern: "^(.{8,16}|)$" }}
-        />
-      </Grid>
-      <Button
-        type="submit"
-        role="button"
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Crear Partida
-      </Button>
-    </Box>
-  </Container>
+    <Container>
+      <Box component="form" onSubmit={onSubmit_newGame} >
+        <Grid>
+          <TextField required
+            name="game-name" label="Nombre" variant="standard" helperText="Entre 3 y 12 caracteres"
+            type="text" inputProps={{ maxLength: 12, pattern: game_name_regex }}
+          />
+        </Grid>
+        <Grid>
+          <TextField required
+            name="games-amount" label="Cantidad de simulaciones" variant="standard" helperText="Entre 1 y 200"
+            defaultValue="200" type="text" inputProps={{ maxLength: 3, pattern: games_amount_regex }}
+          />
+        </Grid>
+        <Grid>
+          <TextField required
+            name="rounds-amount" label="Cantidad de rondas" variant="standard" helperText="Entre 1 y 10000"
+            defaultValue="10000" type="text" inputProps={{ maxLength: 5, pattern: rounds_amount_regex }}
+          />
+        </Grid>
+        <Grid>
+          <TextField required
+            name="max-players" label="Máximo de jugadores" variant="standard" helperText="Entre 2 y 4"
+            defaultValue="4" type="text" inputProps={{ maxLength: 1, pattern: max_players_regex }}
+          />
+        </Grid>
+        <Grid>
+          <TextField required
+            name="min-players" label="Mínimo de jugadores" variant="standard" helperText="Entre 2 y 4"
+            defaultValue="2" type="text" inputProps={{ maxLength: 1, pattern: min_players_regex }}
+          />
+        </Grid>
+        <Grid>
+          <TextField
+            name="password" label="Contraseña" variant="standard" helperText="Entre 8 y 16 caracteres"
+            type="text" inputProps={{ maxLength: 16, pattern: password_regex }}
+          />
+        </Grid>
+        <Button
+          type="submit"
+          role="button"
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Crear Partida
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
@@ -108,10 +113,10 @@ function NewGame() {
   return (
     <div>
       <div><NavBar /></div>
-        <div>
-          <h1>Crear partida</h1>
-          <GameForm />
-        </div>
+      <div>
+        <h1>Crear partida</h1>
+        <GameForm />
+      </div>
     </div>
   );
 };
