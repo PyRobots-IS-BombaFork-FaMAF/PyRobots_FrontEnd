25d2dee243f427f513e8665dc2d8570c71255dec
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-20 20:28:20 -0300

    PYR-60 modularize `simulationResult` type

--- src/features/board/SimulationAPI.ts
+++ src/features/board/SimulationAPI.ts
@@ -1,14 +1,13 @@
+export type robotInSimulationResult = {
+  name: string;
+  rounds: [
+    {
+      coords: { x: number; y: number };
+      direction: number;
+      speed: number;
+    }
+  ];
+  cause_of_death?: "robot execution error";
+};
 
-
-export type simulationResult =
-  Array<{
-    name: string
-    rounds: [{
-      coords: { x: number, y: number }
-      direction: number,
-      speed: number
-    }]
-    cause_of_death?: "robot execution error"
-  }>
-
-
+export type simulationResult = Array<robotInSimulationResult>;
