3feae4347dfd1674ca4edb4aadc96b7328a9bf67
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-21 16:00:48 -0300

    PYR-51 Format `Animate` function

--- src/features/board/Animation.tsx
+++ src/features/board/Animation.tsx
@@ -1,37 +1,33 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-export function Animate(renderFrame: (frame: number) => JSX.Element, amount_frames: number): JSX.Element {
-    const [frame, setFrame] = useState(0);
+export function Animate(
+  renderFrame: (frame: number) => JSX.Element,
+  amount_frames: number
+): JSX.Element {
+  const [frame, setFrame] = useState(0);
 
-    const intervalRef: { current?: NodeJS.Timer} = {}
+  const intervalRef: { current?: NodeJS.Timer } = {};
 
-    useEffect(() => {
-      intervalRef.current = getInterval()
-      return () => clearInterval(intervalRef.current)
-    }, [])
-  
-    const getInterval = () => {
-      const progressInterval = setInterval(() => {
-        setFrame((frame) => frame + 1)
-      }, 500)
-      return progressInterval
-    }
-  
-    const animation = () => {
-      setFrame(frame + 1);
-    };
-    
-    useEffect(() => {
-        const interval = setInterval(animation, 1000);
-        return () => clearInterval(interval);
-    }, []);
-    
-    return (
-        <div>
-            {renderFrame(frame)}
-        </div>
-    );
+  useEffect(() => {
+    intervalRef.current = getInterval();
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  const getInterval = () => {
+    const progressInterval = setInterval(() => {
+      setFrame((frame) => frame + 1);
+    }, 500);
+    return progressInterval;
+  };
+
+  const animation = () => {
+    setFrame(frame + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(animation, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div>{renderFrame(frame)}</div>;
 }
-
-
-
