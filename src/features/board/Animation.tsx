ef8ece463f9b7e7fff58b4e203bb426fa86a9e39
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-21 19:04:35 -0300

    PYR-51 Fix `Animate` function

--- src/features/board/Animation.tsx
+++ src/features/board/Animation.tsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export function Animate(
   renderFrame: (frame: number) => JSX.Element,
   amount_frames: number
 ): JSX.Element {
-  const [frame, setFrame] = useState(0);
+  const [frame, setFrame]: [number, React.Dispatch<React.SetStateAction<number>>] = useState(0);
 
-  const intervalRef: { current?: NodeJS.Timer } = {};
+  const intervalRef = useRef<NodeJS.Timer>()
 
   useEffect(() => {
-    intervalRef.current = getInterval();
-    return () => clearInterval(intervalRef.current);
-  }, []);
+    intervalRef.current = getInterval()
+    return () => clearInterval(intervalRef.current)
+  })
 
   const getInterval = () => {
     const progressInterval = setInterval(() => {
-      setFrame((frame) => frame + 1);
-    }, 500);
-    return progressInterval;
-  };
+      setFrame((frame) => frame + 1)
+    }, 500)
+    return progressInterval
+  }
 
   const animation = () => {
     setFrame(frame + 1);
@@ -27,7 +27,7 @@ export function Animate(
   useEffect(() => {
     const interval = setInterval(animation, 1000);
     return () => clearInterval(interval);
-  }, []);
+  });
 
   return <div>{renderFrame(frame)}</div>;
 }
