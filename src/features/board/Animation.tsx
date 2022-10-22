8d9c275c5d32fe6f1ea3f398cbc620d88fec2972
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-10-22 08:34:25 -0300

    PYR-51 Add documentation to `Animate` function

--- src/features/board/Animation.tsx
+++ src/features/board/Animation.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 
+/** Show `renderFrame(i)` in the `i`th frame. */
 export function Animate(
   renderFrame: (frame: number) => JSX.Element
 ): JSX.Element {
