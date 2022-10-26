762f9b27f02b6d234e80bdde40221a904719e7f5
Santiagocoggiola <santiago.coggiola@mi.unc.edu.ar>
2022-10-26 18:13:01 -0300

    PYR-62 changes to list game style

--- src/features/listMatches/ListMatches.tsx
+++ src/features/listMatches/ListMatches.tsx
@@ -8,226 +8,176 @@ import {
 } from "./ListMatchesApi";
 import {
   Button,
-  Checkbox,
   CssBaseline,
-  FormControlLabel,
-  TextField,
-  Typography,
 } from "@mui/material";
 import NavBar from "../directories/NavBar";
 import "../directories/Home.css";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
+import {GridToolbarFilterButton, GridToolbarContainer, DataGrid, GridColDef} from '@mui/x-data-grid';
 
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
+function CustomToolBar() : JSX.Element  {
+  return(<GridToolbarContainer>
+    <GridToolbarFilterButton/>
+    <Button
+        type="submit"
+        fullWidth
+        form='my-form'
+        variant="contained"
+        data-testid="submit"
+        sx={{
+          maxWidth: 100,
+          mt: 2,
+          mb: 2,
+          ml: 2,
+          backgroundColor: "#43B647",
+          "&:hover": { backgroundColor: "#43B647", boxShadow: "0rem 0.1rem 0.5rem #0d8f11" }
+        }}        
+    >
+            Refresh
+    </Button>
+  </GridToolbarContainer>);
+}
 
 const columns: GridColDef[] = [
   {
     field: 'id',
     headerName: 'ID',
+    headerClassName: "columnClass",
     width: 200,
     editable: false,
+    hide: true,
   },
   {
     field: '_name',
     headerName: 'Nombre',
+    headerClassName: "columnClass",
     width: 200,
     editable: false,
   },
   {
     field: '_rounds',
     headerName: 'Rondas',
+    headerClassName: "columnClass",
     type: 'number',
-    width: 200,
+    width: 100,
     editable: false,
   },
   {
     field: '_games',
     headerName: 'Juegos',
+    headerClassName: "columnClass",
     type: 'number',
-    width: 200,
+    width: 100,
     editable: false,
   },
   {
     field: '_max_players',
     headerName: 'Maxima Cantidad Jugadores',
+    headerClassName: "columnClass",
     width: 200,
     editable: false,
   },
   {
     field: '_min_players',
     headerName: 'Minima Cantidad Jugadores',
+    headerClassName: "columnClass",
     width: 200,
+    editable: false,
+  },
+  {
+    field:'_creator',
+    headerName: "Creador",
+    headerClassName: "columnClass",
+    width: 150,
     editable: false,
   },
   {
     field: '_private',
     headerName: 'Privado',
-    width: 200,
+    headerClassName: "columnClass",
+    width: 100,
+    editable: false,
   },
 ];
 
+
 function callApiList(
   filters: ListMatchesFilter,
-  setMatches: Function,
-  setIsReady: Function
+  setMatches: Function
 ): void {
   const promise1 = Promise.resolve(
     listMatchesApi(filters, localStorage.getItem("access_token")?.toString()!)
   );
   promise1.then((value) => {
     setMatches(value);
-
-    setIsReady(true);
   });
 }
 
 
 export default function ListMatches(): JSX.Element {
   const [matches, setMatches] = useState<ListMatch>([]);
-  const [isReady, setIsReady] = useState(false);
   useEffect(() => {
-    if (isReady) {
-      callApiList({}, setMatches, setIsReady);
-    }
+    callApiList({}, setMatches);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const check = JSON.parse(
-      JSON.stringify(Object.fromEntries(data)),
-      (key, value) => (value === null || value === "" ? undefined : value)
-    );
-
-    if (
-      data.get("game_name")?.toString().length! >= 3 ||
-      data.get("game_name")?.toString()! === ""
-    ) {
-      callApiList(check, setMatches, setIsReady);
-    }
+    callApiList({}, setMatches);
   };
 
+  
   const theme = createTheme();
 
   return (
     <div>
       <NavBar />
-      <div className="bg-image">
+      <div className="bg-image">  
         <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                marginRight: 10,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Box
-                component="form"
-                onSubmit={handleSubmit}
-                noValidate
-                sx={{
-                  mt: 20,
-                  mpl: 2,
-                  backgroundColor: "#f2f2f2",
-                  padding: 5,
-                  mb: 10,
-                  borderRadius: "5%",
-                }}
-              >
-                <h1>Listar Partidas</h1>
-                <TextField
-                  sx={{ maxWidth: 800 }}
-                  autoComplete="Filtrar por nombre"
-                  name="game_name"
-                  fullWidth
-                  data-testid="filterByName"
-                  id="filterByName"
-                  label="Filtrar por nombre"
+           
+          <Container component="main">
+              <CssBaseline />
+                <Box
+                  component="form"
+                  onSubmit={handleSubmit}
+                  noValidate
+                  id ="my-form"
                 />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      color="primary"
-                      sx={{
-                        color: "black",
-                        "&.Mui-checked": { color: "green" },
-                      }}
-                      name="create_by_user"
-                      data-testid="createrByUser"
-                    />
-                  }
-                  label={
-                    <Typography color="black" fontSize="12">
-                      Mis partidas
-                    </Typography>
-                  }
-                  sx={{ mr: 40 }}
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      color="primary"
-                      sx={{
-                        color: "black",
-                        "&.Mui-checked": { color: "green" },
-                      }}
-                      name="only_private"
-                      data-testid="onlyPrivate"
-                    />
-                  }
-                  label={
-                    <Typography color="black" fontSize="12">
-                      Privado
-                    </Typography>
-                  }
-                  sx={{ mr: 40 }}
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  data-testid="submit"
-                  sx={{
-                    maxWidth: 800,
-                    mt: 3,
-                    mb: 2,
+            </Container>
+            <Container 
+              sx = {{
+                display: 'flex', 
+                justifyContent: 'center', 
+                pt: 20,
+                borderStyle:"none",
+              }} >
+            { matches.length > 0 ? ( 
+                  <Box sx={{height: "60vh", width: 1050, maxWidth: "80vw", bgcolor: "background.paper", borderRadius: "5%", '& .columnClass': {
                     backgroundColor: "#43B647",
-                    "&:hover": { backgroundColor: "#43B647", boxShadow: "0rem 0.1rem 0.5rem #0d8f11" }
-                  }}
-                >
-                  Buscar
-                </Button>
-              </Box>
-            </Box>
-          </Container>
-              {isReady ? ( matches.length > 0 ? ( 
-                <Box sx={{ height: "50vh", width: "100vw", maxWidth: "100vw", bgcolor: "background.paper",}}>
-                <DataGrid
-                  rows={matches.map((elem, index) => ({
-                    id: index,
-                    _name : elem._name,
-                    _rounds : elem._rounds,
-                    _games : elem._games,
-                    _max_players: elem._max_players,
-                    _min_players : elem._min_players,
-                    _private : elem._private
-                  }))}
-                  columns={columns}
-                  pageSize={5}
-                  rowsPerPageOptions={[5]}
-                  disableSelectionOnClick
-                  experimentalFeatures={{ newEditingApi: true }}
-                />
-              </Box>) : (
-                <div></div>
-              )) : 
-                <div></div>
-              }
-        </ThemeProvider>
+                  },}}>
+                  <DataGrid
+                    rows={matches.map((elem, index) => ({
+                      id: index,
+                      _name : elem._name,
+                      _rounds : elem._rounds,
+                      _games : elem._games,
+                      _max_players: elem._max_players,
+                      _min_players : elem._min_players,
+                      _creator : elem._creator,
+                      _private : elem._private
+                    }))}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5]}
+                    disableSelectionOnClick
+                    experimentalFeatures={{ newEditingApi: true }}
+                    components = {{Toolbar:CustomToolBar,}}
+                  />
+                </Box>) : (
+                  <div></div>
+                )}
+            </Container>   
+          </ThemeProvider>
       </div>
     </div>
   );
