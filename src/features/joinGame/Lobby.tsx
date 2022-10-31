4c24f763bc7d8d2442f244fe4fdc27394dd0aa70
Franco Artico <francoartico0@gmail.com>
2022-10-31 19:13:49 -0300

    PYR-62 align fix

--- src/features/joinGame/Lobby.tsx
+++ src/features/joinGame/Lobby.tsx
@@ -1,7 +1,5 @@
 import Avatar from "@mui/material/Avatar";
 import Stack from "@mui/material/Stack";
-import Paper from "@mui/material/Paper";
-import { Container } from "@mui/system";
 import { Button, Typography, Grid } from "@mui/material";
 import defaultPlayer from "../../assets/img/defaultPlayer.jpg";
 import defaultRobot from "../../assets/img/defaultRobot.jpg";
@@ -18,7 +16,7 @@ type Props = {
   ];
   setShowLobby: Function;
   isCreator: boolean;
-  socket : WebSocket | undefined;
+  socket: WebSocket | undefined;
 };
 
 const abandoneGame = () => {
@@ -36,136 +34,129 @@ const abandoneGame = () => {
   });
 };
 
-const launchGame = (socket : WebSocket | undefined) => {
-  
-};
-export const Lobby = ({ myKey, players, setShowLobby, isCreator, socket }: Props) => {
+const launchGame = (socket: WebSocket | undefined) => {};
+export const Lobby = ({
+  myKey,
+  players,
+  setShowLobby,
+  isCreator,
+  socket,
+}: Props) => {
   return (
-    <Container key={myKey}>
-      <Grid
-        container
-        sx={{
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          mt: -20,
-        }}
-      >
-        <Paper
-          elevation={3}
-          sx={{
-            width: "40vw",
-            heigth: "100vh",
-            maxHeight: "100vh",
-            borderStyle: "groove",
-            borderRadius: 10,
-            borderColor: "#43B647",
-          }}
-        >
-          <Stack spacing={3} >
-            <Grid item xs={3}></Grid>
-            {players.map((player: any, key: number): any => {
-              return (
-                <Stack
-                  direction="row"
-                  key={key}
-                  sx={{
-                    width: "30vw",
-                    mt: 1,
-                    borderStyle: "double",
-                    borderRadius: 60,
-                    borderColor: "#43B647",
-                  }}
-                >
-                  <Grid item xs={2}>
-                    <Avatar
-                      alt="Player"
-                      src={defaultPlayer}
-                      sx={{ height: "50px", width: "50px" }}
-                    />
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography variant="h6"> {player.player} </Typography>
-                  </Grid>
-                  <Grid item xs={2}></Grid>
-                  <Grid item xs={2}>
-                    <Avatar
-                      alt="Robot"
-                      src={defaultRobot}
-                      sx={{ height: "50px", width: "50px" }}
-                    />
-                  </Grid>
-                  <Grid item xs={2}>
-                    <Typography variant="h6">{player.robot} </Typography>
-                  </Grid>
-                </Stack>
-              );
-            })}
-          </Stack>
-
-          <Stack direction="row" sx={{ mt: 3 }}>
-            <Grid item xs={4}>
-              <Button
-                onClick={(event) => {
-                  socket?.close();
-                  setShowLobby(false);
-                }}
-                variant="contained"
+    <Grid
+      key={myKey}
+      container
+      sx={{
+        width: "50vw",
+        backgroundColor: "white",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        border: "2px groove #43B647",
+        borderRadius: 10,
+      }}
+    >
+      <Grid>
+        <Stack spacing={3}>
+          {players.map((player: any, key: number): any => {
+            return (
+              <Stack
+                direction="row"
+                key={key}
                 sx={{
-                  mt: 3,
-                  mb: 2,
-                  backgroundColor: "#ECE04A",
-                  "&:hover": {
-                    backgroundColor: "#ECE04A",
-                    boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
-                  },
+                  width: "30vw",
+                  mt: 5,
+                  borderStyle: "double",
+                  borderRadius: 60,
+                  borderColor: "#43B647",
                 }}
               >
-                Ir a listar partidas
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button
-                onClick={(event) => {
-                  launchGame(socket);
-                }}
-                variant="contained"
-                sx={{
-                  mt: 3,
-                  mb: 2,
-                  backgroundColor: "#43B647",
-                  "&:hover": {
-                    backgroundColor: "#43B647",
-                    boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
-                  },
-                }}
-              >
-                Iniciar Partida
-              </Button>
-            </Grid>
-            <Grid item xs={4}>
-              <Button
-                variant="contained"
-                sx={{
-                  mt: 3,
-                  mb: 2,
-                  backgroundColor: "#C72603",
-                  "&:hover": {
-                    backgroundColor: "#C72603",
-                    boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
-                  },
-                }}
-                disabled={isCreator}
-                onClick={() => abandoneGame()}
-              >
-                Abandonar Partida
-              </Button>
-            </Grid>
-          </Stack>
-        </Paper>
+                <Grid item xs={2}>
+                  <Avatar
+                    alt="Player"
+                    src={defaultPlayer}
+                    sx={{ height: "50px", width: "50px" }}
+                  />
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="h6"> {player.player} </Typography>
+                </Grid>
+                <Grid item xs={2}>
+                  <Avatar
+                    alt="Robot"
+                    src={defaultRobot}
+                    sx={{ height: "50px", width: "50px" }}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <Typography variant="h6">{player.robot} </Typography>
+                </Grid>
+              </Stack>
+            );
+          })}
+        </Stack>
       </Grid>
-    </Container>
+
+      <Stack direction="row" sx={{ mt: 3 }}>
+        <Grid item xs={4}>
+          <Button
+            onClick={(event) => {
+              socket?.close();
+              setShowLobby(false);
+            }}
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "#ECE04A",
+              "&:hover": {
+                backgroundColor: "#ECE04A",
+                boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
+              },
+            }}
+          >
+            Ir a listar partidas
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            onClick={(event) => {
+              launchGame(socket);
+            }}
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "#43B647",
+              "&:hover": {
+                backgroundColor: "#43B647",
+                boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
+              },
+            }}
+          >
+            Iniciar Partida
+          </Button>
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: "#C72603",
+              "&:hover": {
+                backgroundColor: "#C72603",
+                boxShadow: "0rem 0.1rem 0.5rem #0d8f11",
+              },
+            }}
+            disabled={isCreator}
+            onClick={() => abandoneGame()}
+          >
+            Abandonar Partida
+          </Button>
+        </Grid>
+      </Stack>
+    </Grid>
   );
 };
