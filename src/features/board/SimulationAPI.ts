f346dbec57e6d982e12f51f0d85149b131bc807c
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-11-02 15:10:27 -0300

    PYR-68 Modifications to API of the backend

--- src/features/board/SimulationAPI.ts
+++ src/features/board/SimulationAPI.ts
@@ -1,12 +1,17 @@
 import axios from "../../api/axios";
 
 export type newSimulationInfo = {
-  robot1_id: number,
-  robot2_id: number,
-  robot3_id?: number,
-  robot4_id?: number,
-  amount_rounds: number
-}
+  robot1_id: number;
+  robot2_id: number;
+  robot3_id?: number;
+  robot4_id?: number;
+  amount_rounds: number;
+};
+
+type newSimulationInfoAPI = {
+  robots: Array<{ id: number }>;
+  rounds: { rounds: number };
+};
 
 export type robotInSimulationResult = {
   name: string;
@@ -15,8 +20,8 @@ export type robotInSimulationResult = {
     direction: number;
     speed: number;
     damage: number;
-    missile?: { direction: number, distance: number };
-    scanner?: { direction: number, resolution_in_degrees: number };
+    missile?: { direction: number; distance: number };
+    scanner?: { direction: number; resolution_in_degrees: number };
   }>;
   cause_of_death?: "robot execution error" | "out of life";
 };
@@ -27,13 +32,36 @@ export type simulationResult = {
   robots: Array<robotInSimulationResult>;
 };
 
+function newSimulationInfo_toAPI(
+  newSimulationInfo: newSimulationInfo
+): newSimulationInfoAPI {
+  const res = {
+    robots: [
+      { id: newSimulationInfo.robot1_id },
+      { id: newSimulationInfo.robot2_id },
+    ],
+    rounds: { rounds: newSimulationInfo.amount_rounds },
+  };
+
+  if (newSimulationInfo.robot3_id) {
+    res.robots.push({ id: newSimulationInfo.robot3_id });
+  }
+  if (newSimulationInfo.robot4_id) {
+    res.robots.push({ id: newSimulationInfo.robot4_id });
+  }
+
+  return res;
+}
+
 export function newSimulationAPI(
   newSimulation: newSimulationInfo,
   access_token: string | null
 ): Promise<simulationResult> {
+  const newSimulationInfoAPI = newSimulationInfo_toAPI(newSimulation);
+
   return new Promise((resolve, reject) => {
     axios
-      .post("simulation", newSimulation, {
+      .post("simulation", newSimulationInfoAPI, {
         headers: {
           Authorization: `Bearer ${access_token}`,
           "Content-Type": "application/json",
