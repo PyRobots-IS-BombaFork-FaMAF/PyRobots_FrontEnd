013d8e5559505dae885ab3b23fa5d3ebe5cb3e02
Franco <francoartico0@gmail.com>
2022-11-03 00:07:03 -0300

    PYR-82 tests

--- src/features/results/results.tsx
+++ src/features/results/results.tsx
@@ -16,6 +16,7 @@ import {
 import { useState, useEffect } from "react";
 import axios from "../../api/axios";
 import Pagination from "@mui/material/Pagination";
+import swal from "sweetalert2";
 
 type modalState = {
   modal: boolean;
@@ -117,7 +118,7 @@ const Stats = (props: any) => {
   );
 };
 
-const CardWin = (props: any) => {
+export const CardWin = (props: any) => {
   const { setModal } = React.useContext(ModalState);
 
   const handleClick = (e: any) => {
@@ -139,7 +140,7 @@ const CardWin = (props: any) => {
       }}
     >
       <CardContent>
-        <Typography variant="h4" sx={{ color: "#43B647" }}>
+        <Typography data-testid="text-win" variant="h4" sx={{ color: "#43B647" }} >
           GANASTE
         </Typography>
         <Typography>
@@ -175,7 +176,65 @@ const CardWin = (props: any) => {
   );
 };
 
-const CardLose = (props: any) => {
+export const CardDraw = (props: any) => {
+  const { setModal } = React.useContext(ModalState);
+
+  const handleClick = (e: any) => {
+    setModal(true);
+    props.setIdStats(e.target.getAttribute("id"));
+  };
+
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        minWidth: 275,
+        maxWidth: 300,
+        margin: 3,
+        background: "#EAD99D",
+        boxShadow: 6,
+        border: "2px solid #B7992C",
+        "&:hover": { boxShadow: "0rem 0.5rem 1rem #EAD99D" },
+      }}
+    >
+      <CardContent>
+        <Typography data-testid="text-draw" variant="h4" sx={{ color: "#B7992C" }}>
+          EMPATE
+        </Typography>
+        <Typography>
+          <strong>Robot usado:</strong> {props.robotName}
+        </Typography>
+        <Typography>
+          {" "}
+          <strong>Nombre de partida:</strong> {props.gameName}
+        </Typography>
+        <Typography>
+          {" "}
+          <strong>Fecha de creación:</strong> {props.gameDate}
+        </Typography>
+        <CardActions>
+          <Button
+            onClick={handleClick}
+            type="submit"
+            role="button"
+            variant="contained"
+            data-testid="submit-robot"
+            id={props.index}
+            sx={{
+              width: "100%",
+              backgroundColor: "#D1C51D",
+              "&:hover": { backgroundColor: "#D1C51D" },
+            }}
+          >
+            Estadísticas
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const CardLose = (props: any) => {
   const { setModal } = React.useContext(ModalState);
 
   const handleClick = (e: any) => {
@@ -197,7 +256,7 @@ const CardLose = (props: any) => {
       }}
     >
       <CardContent>
-        <Typography variant="h4" sx={{ color: "#BF0F0F" }}>
+        <Typography data-testid="text-lose" variant="h4" sx={{ color: "#BF0F0F" }}>
           PERDISTE
         </Typography>
         <Typography>
@@ -240,7 +299,7 @@ const HistoricalResults = () => {
   const [results, setResults] = useState<Array<any>>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [resultsPerPage, setResultsPerPage] = useState<number>(25);
+  const [resultsPerPage] = useState<number>(25);
   const [idStats, setIdStats] = useState<number>(0);
   const access_token = localStorage.getItem("access_token")?.toString();
 
@@ -251,17 +310,35 @@ const HistoricalResults = () => {
   useEffect(() => {
     const fetchResults = async () => {
       setLoading(true);
-      const res = await axios.get("game/results", {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      setResults(res.data);
-      setLoading(false);
+      await axios
+        .get("game/results", {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((res) => {
+          setResults(res.data);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          swal.fire({
+            title: "Error",
+            text: error.response.data.detail,
+            icon: "error",
+            confirmButtonColor: "#43B647",
+          });
+          if (error.response.status === 401) {
+            localStorage.clear();
+            setTimeout(() => {
+              window.location.reload();
+            }, 2000);
+          }
+        });
     };
 
     fetchResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // get current results
@@ -270,7 +347,7 @@ const HistoricalResults = () => {
   const currentResult = results.slice(indexOfFirstResult, indexOfLastResult);
 
   const searchName = (players: any) =>
-    localStorage.getItem("username")?.toString() == players.player;
+    localStorage.getItem("username")?.toString() === players.player;
 
   return loading ? (
     <Grid
@@ -293,8 +370,19 @@ const HistoricalResults = () => {
           {currentResult.map((result: any, index: number) =>
             currentResult.length > 0 ? (
               <Grid key={index}>
-                {result.winners[0].name ===
-                localStorage.getItem("username")?.toString() ? (
+                {result.winners.length > 1 &&
+                result.winners.find(searchName).player ===
+                  localStorage.getItem("username")?.toString() ? (
+                  <CardDraw
+                    index={index}
+                    setIdStats={setIdStats}
+                    robotName={result.players.find(searchName).robot}
+                    gameDate={result.creation_date}
+                    gameName={result.name}
+                  />
+                ) : result.winners.length === 1 &&
+                  result.winners.find(searchName).player ===
+                    localStorage.getItem("username")?.toString() ? (
                   <CardWin
                     index={index}
                     setIdStats={setIdStats}
