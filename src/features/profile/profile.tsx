e2a87a1e741ebbfbd25420e317544c669b711a24
Franco Artico <francoartico0@gmail.com>
2022-11-20 20:14:25 -0300

    Show user info

--- src/features/profile/profile.tsx
+++ src/features/profile/profile.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "@emotion/react";
 import {
   Avatar,
+  CircularProgress,
   createTheme,
   Divider,
   Grid,
   Stack,
   Typography,
 } from "@mui/material";
+import { useEffect, useState } from "react";
 import NavBar from "../directories/NavBar";
+import { callApiFetchInfo } from "./profileApi";
 
 const theme = createTheme({
   components: {
@@ -31,7 +34,29 @@ const theme = createTheme({
 });
 
 const Profile = () => {
-  return (
+  const access_token = localStorage.getItem("access_token")?.toString();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [info, setInfo] = useState<any>(null);
+
+  useEffect(() => {
+    callApiFetchInfo(access_token, setInfo, setLoading);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return loading ? (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={3}>
+        <CircularProgress />
+      </Grid>
+    </Grid>
+  ) : (
     <ThemeProvider theme={theme}>
       <NavBar />
       <Grid container>
@@ -44,15 +69,19 @@ const Profile = () => {
             borderRadius: "5px",
           }}
         >
-          {/* <ButtonLoadAvatar /> */}
           <Grid container>
             <Grid item xs="auto">
-              <Avatar variant="rounded" alt="User Avatar" />
+              <Avatar
+                variant="rounded"
+                alt="User Avatar"
+                src={`data:image/${info.avatar_name.split(".")[1]};base64,${
+                  info.avatar_img.split("'")[1].split("'")[0]
+                }`}
+              />
             </Grid>
             <Grid item xs="auto" sx={{ margin: "auto" }}>
               <Typography variant="h3" sx={{ margin: "auto" }}>
-                {" "}
-                Hola Usuario!{" "}
+                Hola {info.name}!
               </Typography>
             </Grid>
             <Grid item xs={12}>
@@ -71,9 +100,14 @@ const Profile = () => {
                 divider={<Divider variant="middle" />}
                 sx={{ margin: "20px 50px 0 50px" }}
               >
-                <Typography> Nombre de usuario: </Typography>
-                <Typography> Dirección de correo: </Typography>
-                <Typography> Constraseña: </Typography>
+                <Typography>
+                  {" "}
+                  Nombre de usuario: <strong>{info.name}</strong>
+                </Typography>
+                <Typography>
+                  {" "}
+                  Dirección de correo: <strong>{info.email}</strong>
+                </Typography>
               </Stack>
             </Grid>
           </Grid>
