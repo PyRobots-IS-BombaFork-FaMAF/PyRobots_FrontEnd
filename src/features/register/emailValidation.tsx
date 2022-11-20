3193b8b3f0708b152d78e97d204455addb62560c
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-11-20 13:49:54 -0300

    PYR-110 Add go back buttons to email validation page

--- src/features/register/emailValidation.tsx
+++ src/features/register/emailValidation.tsx
@@ -1,36 +1,73 @@
+import { Button } from "@mui/material";
 import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import {
+  NavigateFunction,
+  useNavigate,
+  useSearchParams,
+} from "react-router-dom";
 
 import { emailValidationAPI } from "./emailValidationAPI";
 
+function GoBackButtons(): JSX.Element {
+  const navigate: NavigateFunction = useNavigate();
+  return (
+    <div>
+      <Button onClick={() => navigate("/login", { replace: true })}>
+        Iniciar sección
+      </Button>
+      <Button onClick={() => navigate("/register", { replace: true })}>
+        Registrarse
+      </Button>
+    </div>
+  );
+}
+
 function InvalidArgumentsPage(): JSX.Element {
-  return <h1>Argumentos inválidos</h1>;
+  return (
+    <div>
+      <h1>Argumentos inválidos</h1>
+      <GoBackButtons />
+    </div>
+  );
 }
 
 function SuccessPage(): JSX.Element {
-  return <h1>Cuenta validada correctamente</h1>;
+  return (
+    <div>
+      <h1>Cuenta validada correctamente</h1>
+      <GoBackButtons />
+    </div>
+  );
 }
 
 function ErrorPage(): JSX.Element {
-  return <h1>Email o código incorrectos</h1>;
+  return (
+    <div>
+      <h1>Email o código incorrectos</h1>
+      <GoBackButtons />
+    </div>
+  );
 }
 
 function EmailValidationPage(): JSX.Element {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, _] = useSearchParams();
   const [state, setState] = useState<
-    null | "invalid arguments" | "success" | "error"
+    null | "invalid arguments" | "waiting" | "success" | "error"
   >(null);
 
   const email: string | null = searchParams.get("email");
   const code: string | null = searchParams.get("code");
 
   if (email === null || code === null) {
-    setState("invalid arguments");
-  } else {
+    if (state !== "invalid arguments") {
+      setState("invalid arguments");
+    }
+  } else if (state === null) {
     const validation: Promise<void> = emailValidationAPI({
       email: email,
       code: code,
     });
+    setState("waiting");
 
     validation
       .then(() => {
